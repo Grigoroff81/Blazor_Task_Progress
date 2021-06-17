df6b86c75ccfcb69f50347969918b025edda730e
Grigoroff81 <ivogrigoroff.gmail.com>
2021-06-17 18:48:41 +0300

    product with 0 AvailablePsc has Product.Picture and out of stock picture

--- AutoSparePartsE-shop/Data/Product.cs
+++ AutoSparePartsE-shop/Data/Product.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
 
 namespace AutoSparePartsE_shop.Data
 {
